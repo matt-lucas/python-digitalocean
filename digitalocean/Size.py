33aa8a6069e0cccf5502c7e5620e8277a71e5ecd
Matt Lucas <matt@unspoil.com>
2013-08-28 01:29:17 +0000

    Added Size.all() function to get all sizes.

--- digitalocean/Size.py
+++ digitalocean/Size.py
@@ -4,6 +4,15 @@ class Size(object):
     def __init__(self, client_id="", api_key=""):
         self.client_id = client_id
         self.api_key = api_key
-
         self.name = None
         self.id = None
+
+    def all(self):
+        """
+            Get all Sizes
+        """
+        payload = {'client_id': self.client_id, 'api_key': self.api_key}
+        r = requests.get("https://api.digitalocean.com/sizes/", params=payload)
+        return r.json()
+
+        
